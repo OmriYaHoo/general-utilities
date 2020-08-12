3f8dc303351ff426602602e0914481840585f88a
Omri Ratson <omriyahoo@gmail.com>
2020-08-12 10:02:17 +0300

    Update clone_repos_in_organization.sh
    adding windows example

--- clone_repos_in_organization.sh
+++ clone_repos_in_organization.sh
@@ -1,12 +1,18 @@
-# Compatible with UNIX machines (Mac, Linux)
+# Compatible with UNIX machines (Mac, Linux) terminal and windows git-bash
 # This method assumes you have ssh keys set up in github and your local machine
-# You may need first to install 'jq' and 'parallel' packages - in case your permissions do not allow parallel cloning, remove the 'parallel' keyword from the command
+# You may need first to install 'jq' package
+# in case of windows download the jq executable from here https://github.com/stedolan/jq/releases  and point the command to the location
 
 # From public GitHub
-curl -s https://$THE_USER_NAME:$THE_USER_PASS@api.github.com/orgs/$ORGANIZATION_NAME/repos?per_page=200 | jq .[].ssh_url | parallel xargs -n
+curl -s https://$THE_USER_NAME:$THE_USER_PASS@api.github.com/orgs/$ORGANIZATION_NAME/repos?per_page=200 | jq .[].ssh_url | xargs -n
  1 git clone
 
 
 # From company GitHub
-curl -s https://$THE_USER_NAME:$THE_USER_PASS@api.github.$THE_COMPANY_DOMAIN.com/orgs/$ORGANIZATION_NAME/repos?per_page=200 | jq .[].ssh_url | parallel xargs -n
+curl -s https://$THE_USER_NAME:$THE_USER_PASS@api.github.$THE_COMPANY_DOMAIN.com/orgs/$ORGANIZATION_NAME/repos?per_page=200 | jq .[].ssh_url | xargs -n
+ 1 git clone
+
+
+# Example of windows - see the 'jq' part
+curl -s https://$THE_USER_NAME:$THE_USER_PASS@api.github.com/orgs/$ORGANIZATION_NAME/repos?per_page=200 | ./jq-win64.exe .[].ssh_url | xargs -n
  1 git clone
